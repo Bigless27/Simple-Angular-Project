aab7aedc97ec154fd3670f5dc87a75f8398be15b
Bigless27 <lessonryan@gmail.com>
2016-02-19 14:49:31 -0500

    Add route configuration for partials

--- Angular-search/root.js
+++ Angular-search/root.js
@@ -1,10 +1,34 @@
 
 var searchApp = angular.module('searchApp', []);
 
+searchApp.config(function($routeProvider){
+  $routeProvider
+  .when('/',
+  {
+    controller: 'SimpleController',
+    templateUrl: 'Partials/View1.html'
+  })
+  .when('/view2',
+  {
+    controller: 'SimpleController',
+    templateUrl: "Partials/View2.html"
+  })
+  .otherwise({redirectTo: '/'});
+});
+
 searchApp.controller('SimpleController', function($scope){
   $scope.customers = [
   {name: 'Gendelman', city: 'West Bloomfield'},
   {name: 'Ryan Lesson', city: "Chicago"},
-  {name: 'Corey Lesson', city: "Bloomfield Hills"}
+  {name: 'Corey Lesson', city: "Bloomfield Hills"},
+  {name: "Ash Ketchum", city: "Pallet Town"},
+  {name: "Videl", city: "Hercule City"},
+  {name: "Josh Lesson", city: "Miami Beach"}
   ];
+
+  $scope.addCustomer = function(){
+    $scope.customers.push({name: $scope.newCustomer.name , city: $scope.newCustomer.city})
+  }
 });
+
+
