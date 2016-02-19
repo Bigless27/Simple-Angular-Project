5276ea86a8fc5db9450061bf2e590a03d970e1e6
Bigless27 <lessonryan@gmail.com>
2016-02-18 23:58:35 -0500

    create list of objects to search throught

---
+++ Angular-search/root.js
@@ -0,0 +1,10 @@
+var searchApp = angular.module('searchApp', {});
+
+searchApp.controller('SimpleController', function($scope){
+  $scope.customers = [
+  {name: 'Gendelman', city: 'West Bloomfield'},
+  {name: 'Ryan Lesson', city: "Chicago"},
+  {name: 'Corey Lesson', city: "Bloomfield Hills"}
+  ];
+});
+
